e3d574673733c758d50a7ce0a7868542cb2948a8
crocusia <132359536+crocusia@users.noreply.github.com>
2025-04-17 20:33:55 +0900

    refactor : Lv2 N+1 Problem

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +12,14 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    //할일과 유저는 ManyToOne 관계이다
+    //1개의 쿼리문으로 N개의 할일을 조회하고 N개의 할일에 대한 유저를 조회하며 N+1 문제가 발생하게 된다
+    //Fetch Join은 연관관계를 한 번에 조회하여 N+1 문제를 방지한다
+    //@Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+
+    //EntityGrape는 Fetch Join을 직접 작성하지 않아도 된다.
+    @Query("SELECT t FROM Todo t ORDER BY t.modifiedAt DESC")
+    @EntityGraph(attributePaths = {"user"})
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
